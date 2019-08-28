acef55ec6f7fd26728a273f5ff57dff2339b74fc
vinistrauss <vini.strauss@hotmail.com>
2019-08-28 00:22:05 -0300

    adicionando arquivos para colocar no ar nossa aplicação

--- src/server.js
+++ src/server.js
@@ -1,50 +1,47 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const routes = require('./routes');
-
+const routes = require("./routes");
 
 const app = express();
-const server = require('http').Server(app);
+const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-const connectedUsers = {}
+const connectedUsers = {};
 
-io.on('connection', socket => {
-    //console.log('nova conexão', socket.id);
+io.on("connection", socket => {
+  //console.log('nova conexão', socket.id);
 
-    // socket.on('hello', message => {
-    //     console.log(message);
-    // })
+  // socket.on('hello', message => {
+  //     console.log(message);
+  // })
 
-    // setTimeout(() => {
-    //     socket.emit('world', {
-    //         message: 'vinistrauss'
-    //     });
-    // }, 5000)
+  // setTimeout(() => {
+  //     socket.emit('world', {
+  //         message: 'vinistrauss'
+  //     });
+  // }, 5000)
 
-    const {user} = socket.handshake.query;
-    //console.log(user, socket.id)
-    connectedUsers[user] = socket.id;
-
-
+  const { user } = socket.handshake.query;
+  //console.log(user, socket.id)
+  connectedUsers[user] = socket.id;
 });
 
-mongoose.connect('mongodb+srv://vinistrauss:vinicius@cluster0-u0dek.mongodb.net/tindev?retryWrites=true&w=majority', {useNewUrlParser:true});
-
+mongoose.connect(
+  "mongodb+srv://vinistrauss:vinicius@cluster0-u0dek.mongodb.net/tindev?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 app.use((req, res, next) => {
-    req.io = io;
-    req.connectedUsers = connectedUsers;
+  req.io = io;
+  req.connectedUsers = connectedUsers;
 
-    return next();
-})
-
+  return next();
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-
-server.listen(3333);
+server.listen("https://api-tindev-node.herokuapp.com/");
